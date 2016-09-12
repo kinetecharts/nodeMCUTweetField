f2449f24b98e11c160759d7db2cdb5b5cd31eca6
Weidong Yang <weidong@kineviz.com>
2016-09-12 01:16:01 -0700

    arduino patch was in wrong folder. corrected

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -16,9 +16,24 @@ extern "C" {  //required for read Vdd Voltage
   // uint16 readvdd33(void);
 }
 
-int ID = 12;
-int PIN_RELAY=4; //actually 2
-int PIN_LED=5;
+#define D0 16
+#define D1 5 // I2C Bus SCL (clock)
+#define D2 4 // I2C Bus SDA (data)
+#define D3 0
+#define D4 2 // Same as "LED_BUILTIN", but inverted logic
+#define D5 14 // SPI Bus SCK (clock)
+#define D6 12 // SPI Bus MISO 
+#define D7 13 // SPI Bus MOSI
+#define D8 15 // SPI Bus SS (CS)
+#define D9 3 // RX0 (Serial console)
+#define D10 1 // TX0 (Serial console)
+
+int ID = 0;   //change this for each device
+
+int PIN_RELAY=D1; //5
+int PIN_R=D2;   //4
+int PIN_G=D3;   //0
+int PIN_B=D4;   //2
 
 int status = WL_IDLE_STATUS;
 const char* ssid = "kineviz_test";  //  your network SSID (name)
@@ -41,7 +56,9 @@ WiFiUDP Udp;
 void setup()
 {
   pinMode(PIN_RELAY, OUTPUT);
-  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_R, OUTPUT);
+  pinMode(PIN_G, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
   while (!Serial) {
@@ -72,13 +89,41 @@ void setup()
   Serial.print("Udp server started at port ");
   Serial.println(localPort);
   Udp.begin(localPort);
-  digitalWrite(PIN_RELAY, HIGH);
-  digitalWrite(PIN_LED, HIGH);
-  delay(1000);
-  digitalWrite(PIN_RELAY, LOW);
-  digitalWrite(PIN_LED, LOW);
 
+  blink(255, 0, 0, 0, 500);
+  delay(500);
+  blink(0, 255, 0, 1, 500);
+  delay(500);
+  blink(0, 0, 255, 0, 500);
+  delay(500);
+  blink(255, 255, 255, 1, 500);
+  
   timer.setInterval(3000, heartBeat);
+}
+
+void blink(int r, int g, int b, int relay, int duration){
+    Serial.println(r);
+    Serial.println(g);
+    Serial.println(b);
+    Serial.println(relay);
+    Serial.println();
+
+  
+    if(relay>0) digitalWrite(PIN_RELAY, HIGH);
+    analogWrite(PIN_R, r);
+    analogWrite(PIN_G, g);
+    analogWrite(PIN_B, b);
+//    if(r>0) digitalWrite(PIN_R, HIGH);
+//    if(g>0) digitalWrite(PIN_G, HIGH);
+//    if(b>0) digitalWrite(PIN_B, HIGH);
+    delay(duration);
+    digitalWrite(PIN_RELAY, LOW);
+    analogWrite(PIN_R, 0);
+    analogWrite(PIN_G, 0);
+    analogWrite(PIN_B, 0);  
+//    digitalWrite(PIN_R, LOW);
+//    digitalWrite(PIN_G, LOW);
+//    digitalWrite(PIN_B, LOW);  
 }
 
 void loop()
@@ -86,7 +131,7 @@ void loop()
   timer.run();
 
   int noBytes = Udp.parsePacket();
-  String received_command = "";
+  String cmd = "";
   
   if ( noBytes ) {
     Serial.print(millis() / 1000);
@@ -103,7 +148,7 @@ void loop()
     for (int i=1;i<=noBytes;i++)
     {
       Serial.print(packetBuffer[i-1],HEX);
-      received_command = received_command + char(packetBuffer[i - 1]);
+      cmd = cmd + char(packetBuffer[i - 1]);
       if (i % 32 == 0)
       {
         Serial.println();
@@ -122,19 +167,20 @@ void loop()
 //    Udp.println(ID);
 //    Udp.endPacket();
     
-    Serial.println(received_command);
+    Serial.println(cmd);
     Serial.println();
     
-    digitalWrite(PIN_RELAY, HIGH);
-    digitalWrite(PIN_LED, HIGH);
-    delay(100);
-    digitalWrite(PIN_RELAY, LOW);
-    digitalWrite(PIN_LED, LOW);
-    
+    int commaIndex = cmd.indexOf(',');
+    int secondCommaIndex = cmd.indexOf(',', commaIndex+1);
+    int thirdCommaIndex = cmd.indexOf(',', secondCommaIndex+1);
+    String strR = cmd.substring(0, commaIndex);
+    String strG = cmd.substring(commaIndex+1, secondCommaIndex);
+    String strB = cmd.substring(secondCommaIndex+1, thirdCommaIndex);
+    String strRelay = cmd.substring(thirdCommaIndex+1);
+    blink(strR.toInt(), strG.toInt(), strB.toInt(), strRelay.toInt(), 100);    
   } // end if
-
-
 }
+
 
 void heartMonitor(){
   int sensorValue = analogRead(A0);
